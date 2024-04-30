6f8e2c6c1612252137903617951a503fc78d6db7
Emily Morse <emilygmorse2017@gmail.com>
2024-04-29 21:02:57 -0400

    finishing implementation of calculator

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ class Calulator {
     }
 
     delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
 
     }
 
@@ -40,20 +41,49 @@ class Calulator {
                 computation = prev + current
                 break
             case '-':
-                computation = prev + current
+                computation = prev - current
                 break
             case '*':
-                computation = prev + current
+                computation = prev * current
                 break
             case '÷':
-                computation = prev + current
-                break    
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
         }
     }
 
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+
+        if(this.operation != null) {
+            this.previousOperandTextElement.innerText = 
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
     }
 }
 
@@ -83,5 +113,15 @@ operationButtons.forEach(button => {
 
 equalsButton.addEventListener('click', button => {
     calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
     calculator.updateDisplay()
 })
