44bc4169c0bcd54df1a55dfac46c0f3ed91402ef
Emily Morse <emilygmorse2017@gmail.com>
2024-06-05 20:44:04 -0400

    adding ability to click theme button and change color theme

--- script.js
+++ script.js
@@ -87,15 +87,70 @@ class Calulator {
     }
 }
 
+class Theme {
+    constructor() {
+        const colorWays = {
+            Pink: {
+                leftGradient: "#FC78FF",
+                rightGradient: "#EEDDFF",
+                outputOpacity: .50
+            },
+            Blue: {
+                leftGradient: "#00AAFF",
+                rightGradient: "#00FF6C",
+                outputOpacity: .65
+            },
+            Green: {
+                leftGradient: "#000000",
+                rightGradient: "#000000",
+                outputOpacity: .0
+            }
+        }
+        this.colorWays = colorWays
+        this.color = this.colorWays.Pink
+        /*if (number == 1) {
+            this.color = pink
+            this.number = 0
+        } else if (number == 0) {
+            this.color = blue
+            this.number = 1
+        } else {
+            this.color = null
+        }*/
+    }
+
+    changeTheme(output) {
+        document.body.style.background = 'linear-gradient( to right, ' + this.color.leftGradient + ', ' + this.color.rightGradient + ')'
+        output.style.background = 'rgba(0,0,0,' + this.color.outputOpacity + ')'
+        if (this.color === this.colorWays.Pink) {
+            this.color = this.colorWays.Blue
+        } else if (this.color === this.colorWays.Blue) {
+            this.color = this.colorWays.Green
+        } else {
+            this.color = this.colorWays.Pink
+        }
+    }
+}
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete')
+const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+const output = document.querySelector('[output]')
 
 const calculator = new Calulator(previousOperandTextElement, currentOperandTextElement)
+//const theme = new Theme(1);
+
+const applyThemeButton = document.querySelector('[data-apply-theme]')
+
+const theme = new Theme()
+//const special = document.querySelector('[special]')
+applyThemeButton.addEventListener('click', () =>{
+    theme.changeTheme(output)
+})
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () =>{
